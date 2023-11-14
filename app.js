95c0e0cfa5855278dd230929ebbde8cc5695e949
YoussefG75 <youssef.gasmii1@gmail.com>
2023-11-14 16:26:03 +0100

    2ndcommit

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const { databaseDB } = require('./database');
 
 require('dotenv').config();
 const port = process.env.PORT || 3333;
-const express = require('express');
+
+const twig = require('twig');
 
 var app = express();
 
 // Connexion à la base de données
 databaseDB();
+
+app.set('view engine', 'twig');
+app.set('views', './views');
+app = express();
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 // Route de base pour tester
 app.get('/', (req, res) => {
